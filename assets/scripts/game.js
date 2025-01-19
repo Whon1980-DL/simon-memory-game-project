38daaa7b9eb07ea0d1a6fca7e12b86660895ec78
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2025-01-19 08:20:58 +0000

    Add code to test newGame function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -2,9 +2,28 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: [],
+    choices: ["button1", "button2", "button3", "button4"]
 }
 
-module.exports = { game };
+function newGame() {
+    game.score = 0;
+    game.playerMoves =[];
+    game.currentGame = [];
+    showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function showScore() {
+      document.getElementById("score").innerText = game.score
+}
+
+module.exports = { game, newGame, showScore, addTurn };
 // why the curly braces? Well this is because we'll be exporting more than  
 // one object and function from this file,  so we need to put them in curly braces. 
+
--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game")
+const { game, newGame, showScore, addTurn } = require("../game")
 
 beforeAll(() => {
     let fs = require("fs");
@@ -24,5 +24,32 @@ describe("game object contains correct keys", () => {
     })
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    })
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"])
+    });
+});
+
+describe("newGame works correctly", () => {
+    // I want to use another beforeAll  function, because we want to set  
+    // up the game state with some fake values to  see if the new game function resets them.
+    beforeAll(() => {
+        game.score = 42
+        game.playerMoves = ["button1", "button2"]
+        game.currentGame = ["button1", "button2"]
+        document.getElementById("score").innerText = "42";
+        newGame()
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the player moves array", () =>{
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1)
+    })
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0)
     })
 });
